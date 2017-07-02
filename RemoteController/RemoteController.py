2ae3656a39855b05850b297a11915807350c649d
Leandro Kieliger <leandro.kieliger@epfl.ch>
2017-07-02 19:27:46 +0200

    Implement navigation data byte-packing

--- RemoteController/RemoteController.py
+++ RemoteController/RemoteController.py
@@ -2,6 +2,7 @@ from xbox import Joystick
 
 import time
 import math
+import struct
 
 DATA_FP_FORMAT = '{0:.2f}'
 
@@ -26,7 +27,42 @@ class NavigationData(object):
                        ' THRUST: ',
                        DATA_FP_FORMAT.format(self.thrust)])
 
-    def toBitSequence(self):
+    def toByteSequence(self):
+        """
+        Pack the navigation data values into a byte sequence.
+        Yaw, pitch, roll and thrust values are coded using a signed
+        byte. This allows for values from -128 to +127.
+
+        In normal mode (directions are what a pilot would see if he could sit inside the quadcopter, forward facing)
+        
+            -positive values for yaw means turning clockwise in the same plane as the ground
+             while negative values denote a rotation in the opposite direction.
+             
+            -positive values for pitch means pointing the nose of the quadcopter downwards
+             while negative values denote a rotation in the opposite direction.
+             
+            -positive values for roll means clockwise rotation along the forward axis
+             while negative values denote a rotation in the opposite direction.
+             
+            -positive values for thrust means a general increase in motor rotational speed
+             while negative values are meant for deceleration. Note that this means that
+             the motors could actually be running at full speed while the thrust value is at 0.
+            
+        In acrobatic mode:
+            [not yet defined]
+
+        Integer value:
+        bytes: [0      1      2      3     ]
+               [yaw    pitch  roll   thrust]
+        """
+        data = (self.yaw, self.pitch, self.roll, self.thrust)
+
+        # Converts the values in [-1, 1] interval into the [-128, 127] interval
+        data = tuple(map(lambda x: int(127.5 * x - 0.5), data))
+
+        return struct.pack('4b', *data)
+
+    def _toSignedByte(self):
         pass
 
 class RemoteController(object):
@@ -58,9 +94,11 @@ class RemoteController(object):
                 self.enabled = False
             
             print(str(data))
+            print(str(data.toByteSequence()))
             time.sleep(1.0 / self.refreshRate)
 
         print('Halting remote controller')
+        self.inputDevice.close()
                 
 
 # Ease functions
@@ -74,7 +112,6 @@ def smoothStep(x1):
 def main():
     controller = RemoteController(linearStep)
     controller.run()
-    controller.close()
         
     
 if __name__ == '__main__':
