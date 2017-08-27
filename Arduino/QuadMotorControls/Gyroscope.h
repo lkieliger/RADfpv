d3d302f8ec7d59e63cd9c81075396024e1acb722
lkieliger <leandro.kieliger@epfl.ch>
2017-08-27 23:50:57 +0200

    Remove unnecessary variables in the Gyro class

--- Arduino/QuadMotorControls/Gyroscope.h
+++ Arduino/QuadMotorControls/Gyroscope.h
@@ -32,13 +32,7 @@ class Gyroscope{
 
     // orientation/motion vars
     Quaternion q;           // [w, x, y, z]         quaternion container
-    VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-    VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-    VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-    VectorFloat gravity;    // [x, y, z]            gravity vector
-    float euler[3];         // [psi, theta, phi]    Euler angle container
-    
-    //TODO: simplify with variables yaw / pitch / roll
+    VectorFloat gravity;    // [x, y, z]            gravity vector 
     float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
   public:
